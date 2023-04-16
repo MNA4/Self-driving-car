919b8f3a21eb4aeab69719bf3215c66d75be5d2c
MNA4 <116416379+MNA4@users.noreply.github.com>
2023-04-16 11:58:43 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from numpy import genfromtxt
 import pygame, math, numpy
 from pygame.locals import *
 pygame.init()
@@ -42,7 +43,7 @@ class NeuralNetwork:
     
 class Car:
     def __init__(self, pos, angle):
-        self.img = pygame.transform.scale(pygame.image.load('car.png'), (18, 38))
+        
         self.map = surf
         self.pos = pos
         self.angle = angle
@@ -61,11 +62,11 @@ class Car:
         self.pos[1] -= c
 
     def render(self, screen):
-        rotated_car = pygame.transform.rotozoom(self.img, self.angle, 1)
+        rotated_car = pygame.transform.rotozoom(carimg, self.angle, 1)
         screen.blit(rotated_car, [self.pos[0]-rotated_car.get_width()/2,
                                   self.pos[1]-rotated_car.get_height()/2,])
     def is_colliding(self):
-        rotated_car = pygame.transform.rotozoom(self.img, self.angle, 1)
+        rotated_car = pygame.transform.rotozoom(carimg, self.angle, 1)
         car_mask = pygame.mask.from_surface(rotated_car)
         return mask.overlap(car_mask, [self.pos[0]-rotated_car.get_width()/2,
                                        self.pos[1]-rotated_car.get_height()/2,])
@@ -99,29 +100,45 @@ def cast_ray(o, d):
         if 0<p[0]<WIDTH and 0<p[1]<HEIGHT and mask.get_at(p):
             pygame.draw.line(screen, (0,0,255), o, p)
             pygame.draw.circle(screen, (0,0,0), p, 2)
-            return i/200
+            return i
         p[0]+=d[0]
         p[1]+=d[1]
     pygame.draw.line(screen, (0,0,255), o, p)
     return 200
-
+def reset(i):
+    global l_cars_count, personal_best, generation, death_count, b_biases, b_weights, t
+    l_cars_count = cars_count
+    personal_best = t * avg_speed 
+    generation += 1
+    death_count = 0
+    b_biases = i.biases
+    b_weights = i.weights              
+    t = 0
+    for j in cars:
+        j.dead = False
+        j.pos = [60, 60]
+        j.angle = -90
+        j.biases = b_biases + (numpy.random.rand(j.neural_network.n_node)*2-1)*mutation_rate
+        j.weights = weights = b_weights + (numpy.random.rand(j.neural_network.n_path)*2-1)*mutation_rate
 WIDTH = 1066
 HEIGHT = 512
-FPS = 1000000
 mutation_rate = 1/100
 avg_speed = 5
 cars_count = 100
-
+carimg = pygame.transform.scale(pygame.image.load('car.png'), (18, 38))
 surf = pygame.image.load('map2.png')
 mask = pygame.mask.from_threshold(surf, (181, 230, 29), (1,1,1))
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 22)
-
-b_biases = 0
-b_weights = 0
-
+try:
+    b_biases = genfromtxt('biases.csv', delimiter = ',')#https://stackoverflow.com/questions/3518778/how-do-i-read-csv-data-into-a-record-array-in-numpy
+    b_weights = genfromtxt('weights.csv', delimiter = ',')
+except:
+    b_biases = 0
+    b_weights = 0
+    
 death_count = 0
 generation = 1
 t = 0
@@ -129,23 +146,23 @@ personal_best = 0
 l_cars_count = cars_count
 cars = []
 for i in range(cars_count):
-    c = Car([60, 43],-90)
+    c = Car([60, 60],-90)
     cars.append(c)
     c.biases = b_biases + (numpy.random.rand(c.neural_network.n_node)*2-1)*mutation_rate
-    c.weights = weights = b_weights + (numpy.random.rand(c.neural_network.n_path)*2-1)*mutation_rate
+    c.weights = b_weights + (numpy.random.rand(c.neural_network.n_path)*2-1)*mutation_rate
     
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-    screen.fill((0,0,0))
+    screen.fill((181, 230, 29))
     screen.blit(surf, (0,0))
     for i in cars:
         if not i.dead:
             s = math.sin(math.radians(i.angle))
             c = math.cos(math.radians(i.angle))
-            rotated_car = pygame.transform.rotozoom(i.img, i.angle, 1)
+            rotated_car = pygame.transform.rotozoom(carimg, i.angle, 1)
             pos = i.pos
             d1 = cast_ray(pos, rotate((s,c), (-.5,-1)))
             d2 = cast_ray(pos, rotate((s,c), (0,-1)))
@@ -158,19 +175,7 @@ while running:
             i.render(screen)
             if i.is_colliding():
                 if death_count >= cars_count-1:
-                    l_cars_count = cars_count
-                    personal_best = t * avg_speed 
-                    generation += 1
-                    death_count = 0
-                    b_biases = i.biases
-                    b_weights = i.weights              
-                    t = 0
-                    for j in cars:
-                        j.dead = False
-                        j.pos = [60, 43]
-                        j.angle = -90
-                        j.biases = b_biases + (numpy.random.rand(j.neural_network.n_node)*2-1)*mutation_rate
-                        j.weights = weights = b_weights + (numpy.random.rand(j.neural_network.n_path)*2-1)*mutation_rate
+                    reset(i)
                 else:
                     l_cars_count -= 1
                     i.dead = True
@@ -178,8 +183,8 @@ while running:
     t+=1
     screen.blit(font.render('Furthest distance: %s pixels'%personal_best, True, (0,0,0)), (10,10))
     screen.blit(font.render('Generation: %s'%generation, True, (0,0,0)), (10,32))
-    screen.blit(font.render('Cars: %s'%l_cars_count, True, (0,0,0)), (10,52))
+    screen.blit(font.render('Cars: %s'%l_cars_count, True, (0,0,0)), (10,54))
     pygame.display.flip()
-    clock.tick(FPS)
 pygame.quit()
-
+#numpy.savetxt('biases.csv', b_biases, delimiter=",")
+#numpy.savetxt('weights.csv', b_weights, delimiter=",") #https://stackoverflow.com/questions/6081008/dump-a-numpy-array-into-a-csv-file
